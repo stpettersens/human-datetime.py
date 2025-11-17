3f2208b24a6378a31813cbd562c717485fb60d4e
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2025-11-17 16:42:15 +0000

    use python build from source for python3.13.a on linux.

--- prebuild.py
+++ prebuild.py
@@ -1,21 +1,14 @@
 import os
 import shutil
 import subprocess
-from getpass import getuser
 
 if __name__ == "__main__":
-    clib = 'gnu'
-    arch = 'x86_64'
     patch = '9'
     if os.name == 'posix':
         if os.system('cat /etc/os-release | grep alpine') == 0:
-            clib = 'musl'
             patch = '5'
 
-        arch = subprocess.check_output(['uname', '-m'], text=True).strip()
-
-    lib = os.path.join('/home', getuser(), '.local', 'uv', 'python',
-    f'cpython-3.13.{patch}-linux-{arch}-{clib}', 'libs', 'python3.13.a')
+    lib = os.path.join('/opt', 'python', f'3.13.{patch}', 'libs', 'python3.13.a')
     if os.name == 'nt':
         lib = os.path.join('C:\\', 'Dev', 'Python313', 'libs', 'python313.lib')
 
